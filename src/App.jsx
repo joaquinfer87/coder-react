82f5d10f88068fb75d90ae841088cc1ec12a6d77
joaquinfer87 <joquinfer100@gmail.com>
2023-05-25 03:30:32 -0300

    contador

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListConteiner from './components/ItemListConteiner/ItemListConteiner';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <>
     <NavBar/>
     <ItemListConteiner greeting="Bienvenidos a Planet Rock"/>
+    <ItemCount inicial={1} stock={12}/>
     </>
   
   );
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState, useEffect} from "react";
+
+const ItemCount = ({ inicial, stock }) => {
+  const [contador, setcontador] = useState(inicial);
+  const [color, setcolor] = useState("black");
+
+  useEffect( ()=>{
+  //     if(contador === 4){
+  //         document.getElementById("boton").style.color = "red"
+  //     }
+
+  //   },[contador])
+  if (contador < 12){
+    setcolor("red");
+  } else {
+    setcolor("black");
+  }
+},[contador])
+
+  const agregarAlCarrito = () => {
+    console.log(`agregado ${contador} items`);
+  };
+
+  const incrementar = () => {
+    if (contador < stock) {
+      setcontador(contador + 1);
+    }
+  };
+
+  const disminuir = () => {
+    if (contador > inicial) {
+      setcontador(contador - 1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={disminuir}>-</button>
+      <p>{contador}</p>
+      <button onClick={incrementar}>+</button>
+
+      <button onClick={agregarAlCarrito} style={{color:color}}>
+        Agregar al carro
+      </button>
+    </div>
+  );
+};
+
+export default ItemCount;
